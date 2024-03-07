1cf9f0b9c5cf84b7d1bc8247ddc940cc1a019817
Chema <josemgarciasanchz@gmail.com>
2024-03-07 10:29:29 +0100

    Versión 2

--- Fecha.java
+++ Fecha.java
@@ -3,12 +3,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Fecha {
-  int año, mes, dia, hora, minuto;
+  int ano, mes, dia, hora, minuto;
   Fecha() {
 		 Calendar calendario=Calendar.getInstance();  //Calendar: clase abstracta
 		 Date fecha=calendario.getTime();
 		 calendario.setTime(fecha);
-		 año=calendario.get(Calendar.YEAR);
+		 ano=calendario.get(Calendar.YEAR);
 		 mes=calendario.get(Calendar.MONTH)+1;
 		 dia=calendario.get(Calendar.DAY_OF_MONTH);
 		 hora=calendario.get(Calendar.HOUR_OF_DAY);
@@ -21,22 +21,27 @@ public class Fecha {
   		int auxhPost= fPosterior.hora*60+fPosterior.minuto;
   		System.out.println(fPosterior.hora+":"+fPosterior.minuto+",,"
   		  +auxh+" ->"+auxhPost);
-  		if (año==fPosterior.año && mes==fPosterior.mes && 
+  		if (ano==fPosterior.ano && mes==fPosterior.mes &&
   				dia==fPosterior.dia && auxhPost<=auxh)
   			ok=true;
   		return ok;	
   	}
-	public Fecha(int año, int mes, int dia, int hora, int minuto) {
-	  this.año = año;
+	public Fecha(int ano, int mes, int dia, int hora, int minuto) {
+	  this.ano = ano;
 	  this.mes = mes;
 	  this.dia = dia;
 	  this.hora = hora;
 	  this.minuto = minuto;
     }
+	public Fecha(int ano, int mes, int dia) {
+		this.ano = ano;
+		this.mes = mes;
+		this.dia = dia;
+	}
 
 	public static void main(String[] args) {
 		Fecha f= new Fecha();
-		System.out.println(f.año+":"+f.mes+":"+f.dia+":"+f.hora+":"+
+		System.out.println(f.ano+":"+f.mes+":"+f.dia+":"+f.hora+":"+
 		f.minuto);
 	}
 
